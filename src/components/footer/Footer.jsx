5d51b5233dfed40698a3920ffd6f16ba7f62c09d
prashkup <prashanthkuppa333@gmail.com>
2020-05-10 15:03:20 +0530

    Add a simple footer

---
+++ src/components/footer/Footer.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+import './Footer.css'
+
+const Footer = () => {
+  return (
+    <footer>
+      <h2>Copyright (c) 2020</h2>
+    </footer>
+  )
+}
+
+export default Footer
