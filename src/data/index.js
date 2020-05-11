09932879c6b0d3109aff360bf87b735e2070fe75
prashkup <prashanthkuppa333@gmail.com>
2020-05-11 18:19:32 +0530

    Add flavor and price data

---
+++ src/data/index.js
@@ -0,0 +1,20 @@
+
+const prices = {
+  'tier': 100,
+  'frosting': 15
+}
+const colors = {
+  'chocolate': '#7b3500',
+  'strawberry': '#fc5a8d',
+  'pineapple': '#ffe12d',
+  'butterscotch': '#c98932'
+}
+const baseColors = {
+  'chocolate': '#9c6f40',
+  'strawberry': '#feaec7',
+  'pineapple': '#ffce2f',
+  'butterscotch': '#d8a662'
+}
+const sprinklesColors = ['white', 'black', 'red', 'blue']
+
+export { colors, prices, baseColors, sprinklesColors }
