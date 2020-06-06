bbef87bb3dedcf8ef35e238232c0e26ad56cf17a
prashkup <prashanthkuppa333@gmail.com>
2020-06-06 19:50:02 +0530

    Add the home view

---
+++ src/views/HomeView.jsx
@@ -0,0 +1,14 @@
+import React, { Fragment } from 'react'
+import { Description , ProductCreator, ProductList } from '../ui-elements'
+
+const HomeView = () => {
+  return (
+    <Fragment>
+      <Description />
+      <ProductList />
+      <ProductCreator />
+    </Fragment>
+  )
+}
+
+export default HomeView
