2dbebe3d5af000236657de3fd41de0311a0f0b92
prashkup <prashanthkuppa333@gmail.com>
2020-06-06 19:59:16 +0530

    Allow adding products to cart

--- src/ui-elements/product/Product.jsx
+++ src/ui-elements/product/Product.jsx
@@ -1,16 +1,17 @@
 import React from 'react'
+import ProductAdder from '../productAdder/ProductAdder'
+
 import './Product.css'
 
-const Product = ({name, id, desc, img}) => {
+const Product = ({name, id, desc, img, price}) => {
   return (
     <div className='product'>
       <h1>{name}</h1>
       <h2>{id}</h2>
       <p>{desc}</p>
+      <p>{price}</p>
       <img src={img} alt='cake' />
-      <button>
-        Add to Cart
-      </button>
+      <ProductAdder />
     </div>
   )
 }
