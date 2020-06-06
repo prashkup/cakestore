7b1dd22c77f44653a875ff668f3d62c79957ffbe
prashkup <prashanthkuppa333@gmail.com>
2020-06-06 19:50:33 +0530

    Add nav links

--- src/ui-elements/navbar/Navbar.jsx
+++ src/ui-elements/navbar/Navbar.jsx
@@ -1,17 +1,28 @@
 import React from 'react'
 import PropTypes from 'prop-types'
+import { Link } from '@reach/router'
 
 import './Navbar.css'
+import ShoppingCart from '../shoppingCart/ShoppingCart'
 
 const Navbar = ({title}) => {
   return (
     <nav className='site-nav'>
       <div className='logo'>
-        <h1><a href='/'>{title}</a></h1>
+      <h1><Link to='/'>{title}</Link></h1>
       </div>
       <ul className='nav-links'>
-        <li className='nav-link'>Cart</li>
-        <li className='nav-link'>Sign In</li>
+        <li className='nav-link'>
+          <Link to='/cart'>
+            <ShoppingCart />
+          </Link>
+        </li>
+        <li className='nav-link'>
+          <Link to='/account'>Account</Link>
+        </li>
+        <li className='nav-link'>
+          <Link to='/about'>About</Link>
+        </li>
       </ul>
     </nav>
   )
