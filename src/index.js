aa5897875baa3e8484121143b7bbfe38b02c49d4
prashkup <prashanthkuppa333@gmail.com>
2020-06-06 20:04:58 +0530

    Configure the redux store

--- src/index.js
+++ src/index.js
@@ -4,9 +4,13 @@ import './index.css';
 import App from './App';
 import * as serviceWorker from './serviceWorker';
 
+import configureStore from './store'
+
+const { store } = configureStore()
+
 ReactDOM.render(
   <React.StrictMode>
-    <App />
+    <App store={store} />
   </React.StrictMode>,
   document.getElementById('root')
 );
---
+++ src/store.js
@@ -0,0 +1,34 @@
+import { 
+  createStore
+} from 'redux'
+import reducer from './ui-elements/productAdder/reducer'
+
+const saveToLocalStorage = (state) => {
+  try {
+    const serializedState = JSON.stringify(state)
+    localStorage.setItem('cakestore-state', serializedState)
+  } catch (error) {
+    console.log(error)
+  }
+}
+
+const loadFromLocalStorage = () => {
+  try {
+    const serializedState = localStorage.getItem('cakestore-state')
+    if (serializedState === null) return undefined
+    return JSON.parse(serializedState)
+  } catch (error) {
+    console.log(error)
+    return undefined
+  }
+}
+
+const persistedState = loadFromLocalStorage()
+
+const configureStore = () => {
+  const store = createStore(reducer, persistedState)
+  store.subscribe(() => saveToLocalStorage(store.getState()))
+  return { store }
+}
+
+export default configureStore
